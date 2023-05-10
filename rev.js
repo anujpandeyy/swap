a425ba29c9db26ed998bd56fa91092eec480ea3c
Anuj Pandey <82930923+anujpandeyy@users.noreply.github.com>
2023-05-10 18:22:23 +0530

    Add files via upload

---
+++ rev.js
@@ -0,0 +1,17 @@
+let a=5;
+let b=10;
+let temp=0;
+console.log("Original Number:");
+console.log(a);
+console.log(b);
+
+temp=a;
+a=b;
+b=temp;
+
+
+
+
+console.log("Swapped Number:");
+console.log(a);
+console.log(b);
